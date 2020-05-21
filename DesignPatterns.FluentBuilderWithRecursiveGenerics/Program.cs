bbff9c067da734ae7c029255efed8bb04f091149
Vyacheslav Yankov <slava.brigida@nixsolutions.com>
2020-05-22 00:30:54 +0300

    Added fluent builder with recursive generics

---
+++ DesignPatterns.FluentBuilderWithRecursiveGenerics/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DesignPatterns.FluentBuilderWithRecursiveGenerics
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+
+        public class Builder : PersonNameAndPositionBuilder<Builder>
+        {
+            
+        }
+
+        public static Builder New => new Builder();
+    }
+
+    abstract class PersonBuilder 
+    {
+        protected Person _person = new Person();
+
+        public Person Build()
+        {
+            return _person;
+        }
+    }
+    
+    class PersonNameBuilder<TSelf> : PersonBuilder
+        where TSelf : PersonNameBuilder<TSelf>
+    {
+        public TSelf WithName(string name)
+        {
+            _person.Name = name;
+            return (TSelf) this;
+        }
+    }
+
+    class PersonNameAndPositionBuilder<TSelf> : PersonNameBuilder<PersonNameAndPositionBuilder<TSelf>>
+        where TSelf : PersonNameAndPositionBuilder<TSelf>
+    {
+        public TSelf WithPosition(string position)
+        {
+            _person.Position = position;
+            return (TSelf) this;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.New.WithPosition(".net lead").WithName("Pes").Build();
+            Console.WriteLine(person);
+        }
+    }
+}
